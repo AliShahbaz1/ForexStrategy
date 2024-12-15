b9069eaaa86de1c32b860e85f8c5698500aa9d9e
AliShahbaz1 <ali444shahbaz@gmail.com>
2024-12-15 11:40:05 -0500

    Update update_case.php
    Option to update a case

---
+++ update_case.php
@@ -0,0 +1,26 @@
+<?php
+include 'database.php';
+
+$id = $_POST['id'];
+$date = $_POST['date'];
+$time = $_POST['time'];
+$buy_sell = $_POST['buy_sell'];
+$win_loss = $_POST['win_loss'];
+$reason = $_POST['reason'];
+
+$sql = "UPDATE cases SET 
+        date = '$date', 
+        time = '$time', 
+        buy_sell = '$buy_sell', 
+        win_loss = '$win_loss', 
+        reason = '$reason' 
+        WHERE id = $id";
+
+if ($conn->query($sql) === TRUE) {
+    header("Location: index.html");
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
